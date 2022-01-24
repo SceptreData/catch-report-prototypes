d15d0c0790e1083b0c3d1751b2eb1172b0e83b2d
David Bergeron <david.f.bergeron@gmail.com>
2022-01-24 15:19:19 -0700

    bit of mobile styling

--- dist/index.c4775257.js
+++ dist/index.c4775257.js
@@ -1,3 +1,41 @@
-console.log('Loaded main .js');
+const viewport = document.querySelector('#list-viewport');
+const addFishBtn = document.querySelector('#add-fish');
+const form = document.querySelector('#list-form');
+const fish = [];
+function updateViewport() {
+    viewport.innerHTML = '';
+    let fishHTML = '';
+    let combinedFish = combineFish(fish);
+    for(const fish1 in combinedFish){
+        fishHTML += `
+      <p>
+        <span class="vp-species">${fish1.toUpperCase()}</span> <span class="vp-quantity">${combinedFish[fish1]}</span>
+      </p>
+    `;
+        viewport.innerHTML = fishHTML;
+    }
+}
+function combineFish() {
+    let fishObj = {
+    };
+    fish.forEach((fish2)=>{
+        if (fishObj[fish2.species]) fishObj[fish2.species] += Number(fish2.quantity);
+        else fishObj[fish2.species] = Number(fish2.quantity);
+    });
+    console.log(fishObj);
+    return fishObj;
+}
+function addFish(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const species = formData.get('list-fish');
+    const quantity = formData.get('list-quantity');
+    fish.push({
+        species,
+        quantity
+    });
+    updateViewport();
+}
+addFishBtn.addEventListener('click', addFish);
 
 //# sourceMappingURL=index.c4775257.js.map
